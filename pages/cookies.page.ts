48f933806ebdd901dcd20c1739fb44c14be987ac
Szymon Rybak <szymon.rybak@vaimo.com>
2026-02-15 12:58:43 +0100

    fix : accept cookies function improvement

--- pages/cookies.page.ts
+++ pages/cookies.page.ts
@@ -1,21 +1,27 @@
-import { Locator, Page } from '@playwright/test';
+import { expect, Locator, Page } from '@playwright/test';
 
 export class CookiesPage {
+  cookieModal: Locator;
   cookiesSettingsButton: Locator;
   cookiesAcceptButton: Locator;
 
   constructor(private page: Page) {
+    this.cookieModal = this.page.locator('.modal-wrap cookie-modal');
     this.cookiesSettingsButton = this.page.getByRole('button', {
       name: 'Nastavení preferencí',
     });
     this.cookiesAcceptButton = this.page.getByRole('button', {
-      name: 'Potvrdit vybrané',
+      name: 'Povolit vše',
     });
   }
 
   async acceptCookies(): Promise<void> {
+    await this.cookiesAcceptButton.click();
+    await expect(this.cookieModal).toBeHidden();
+  }
+
+  async cookiesSettings(): Promise<void> {
     await this.page.waitForLoadState();
     await this.cookiesSettingsButton.click();
-    await this.cookiesAcceptButton.click();
   }
 }
